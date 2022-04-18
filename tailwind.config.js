01e2d4abad1b30cb45c09614d1363a78eb2736e6
Chris <chrisbreuer93@gmail.com>
2022-04-17 22:47:22 -0700

    chore: wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,8 @@
-const plugin = require('tailwindcss/plugin')
-
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,ts}'],
   theme: {
     extend: {}
   },
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    plugin(function ({ addVariant, e, postcss }) {
-      addVariant('firefox', ({ container, separator }) => {
-        const isFirefoxRule = postcss.atRule({
-          name: '-moz-document',
-          params: 'url-prefix()'
-        });
-        isFirefoxRule.append(container.nodes);
-        container.append(isFirefoxRule);
-        isFirefoxRule.walkRules((rule) => {
-          rule.selector = `.${e(
-            `firefox${separator}${rule.selector.slice(1)}`
-          )}`;
-        });
-      });
-    })
-  ]
+  plugins: []
 };
