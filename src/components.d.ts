4e5686f3ab05dd5cbb42052cce606f171bd10b58
Chris <chrisbreuer93@gmail.com>
2022-04-18 16:40:56 -0700

    chore: wip

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,7 +5,6 @@ import '@vue/runtime-core'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./components/HelloWorld.ce.vue')['default'];
     'HelloWorld.ce': typeof import('./components/HelloWorld.ce.vue')['default']
   }
 }
