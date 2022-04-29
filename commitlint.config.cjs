c6b73317f8a54a47da61aee193694d40ba9ea429
Chris <chrisbreuer93@gmail.com>
2022-04-29 09:39:33 -0700

    chore: wip

---
+++ commitlint.config.cjs
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const path = require('path')
+
+const composables = fs.readdirSync(path.resolve(__dirname, 'src/composables'))
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      ['', 'docs', 'example', 'release', 'tooling', 'housecleaning', ...composables.map(item => item.replace(/.ts/g, '')).filter(item => item !== 'index')],
+    ],
+  },
+}
